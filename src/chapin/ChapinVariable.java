3d7e9072ed719c543c640a195ff50b52ff1ece5d
Bakyt Madi <bakyt.madi.work@gmail.com>
2020-11-10 21:40:48 +0300

    Add toString function

--- src/chapin/ChapinVariable.java
+++ src/chapin/ChapinVariable.java
@@ -1,6 +1,6 @@
 package chapin;
 
-class ChapinVariable extends Variable{
+class ChapinVariable extends Variable {
     boolean isIO;
     boolean isModified;
     boolean isInCondition;
@@ -12,5 +12,11 @@ class ChapinVariable extends Variable{
         isModified = false;
         isInCondition = false;
         isUnused = true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("{id:'%s', init:'%s', io:'%s', mod:'%s', cond:'%s', unus:'%s'}",
+                identifier, isInitialized, isIO, isModified, isInCondition, isUnused);
     }
 }
