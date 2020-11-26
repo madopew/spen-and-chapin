5a886ab4de15d61647e571041f621513acbdeb2a
Vladislav <vladmaiski228@gmail.com>
2020-11-26 20:26:56 +0300

    Update small programm

--- res/Small.kt
+++ res/Small.kt
@@ -1,20 +1,17 @@
 fun main(a : Double) {
-    println(a++)
-    var abs = 4
-    val isTrue : Boolean = true
-    val isCond = false
+    val eps : Double = 0.00001
+    var x : Double
+    var y : Double
+    var det : Double
+    var n : Int = 3
+    val PI : Double = 3.14
+    x = a
+    y = a
+    det = readLine()!! + a
     do {
-        isTrue = !false
-        if(abs == 3)
-            print(abs)
-    } while(isCond)
-    val radius = 3.14
-    radius += 1
-    val unused = 4
-    val use = 3
-    print(use++)
-    val something : Integer
-    something = getAbs(Integer.nextInt(getAbs(getAbs(readLine()!!))))
-    val some = 3
-    print(getAbs(1 + getAve(some, foo(use))))
+        det = -det * x * x / ((n + 1)*n)
+        n++
+        y += det
+    } while (getAbs(det) > eps)
+    println(y)
 }
